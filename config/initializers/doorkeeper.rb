9ae6610de8eb257ed44c32581b5455067ee04cd6
David Jeusette <jeusette.david@gmail.com>
2016-04-19 17:31:08 +0200

    Properly configure doorkeeper for password grant

--- config/initializers/doorkeeper.rb
+++ config/initializers/doorkeeper.rb
@@ -2,8 +2,7 @@ Doorkeeper.configure do
   # Change the ORM that doorkeeper will use (needs plugins)
   orm :active_record
 
-  # This block will be called to check whether the resource owner is authenticated or not.
-  resource_owner_authenticator do
+  resource_owner_from_credentials do |routes|
     user = User.find_for_database_authentication(:email => params[:email])
     if user && user.valid_for_authentication? { user.valid_password?(params[:password]) }
       user
